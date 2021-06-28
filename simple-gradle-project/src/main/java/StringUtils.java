26fd3185cb6ddc27e2b0c170f4451c555fa93a93
Lena Youssef <lena.youssef@sky.uk>
2021-06-28 16:48:40 +0100

    new task

--- simple-gradle-project/src/main/java/StringUtils.java
+++ simple-gradle-project/src/main/java/StringUtils.java
@@ -5,8 +5,14 @@ public class StringUtils {
 
 
 /*
- Given a list of strings, return a list of the strings, omitting any string of length 3 or 4.
- [aaa, abba, acca, adda, aa, aaqqa, bb, aaaa, 1234, sky, three, four, ccc, dddd, as@sd, *]  -> [aa,aabba,bb,three,as@sd,*]
- */
+- Task 1
+  Given a list of strings, return a list of the strings, omitting any string of length 3 or 4.
+  [aaa, abba, acca, adda, aa, aaqqa, bb, aaaa, 1234, sky, three, four, ccc, dddd, as@sd, *]  -> [aa,aabba,bb,three,as@sd,*]
+
+ - Task 2
+  Write a method that converts the result of the list strings in a list to their upper case.
+
+
+*/
 
 }
